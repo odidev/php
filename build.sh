c15bb2c9b423c348dd759a96439905357a86b3b1
odidev <odidev@puresoftware.com>
2021-08-17 11:58:44 +0000

    Add ARM64 support using docker buildx

--- build.sh
+++ build.sh
@@ -10,18 +10,14 @@ buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 docker login -u "$2" -p "$3"
 
 # build base images
-docker build -q -t "$1"/php:5.6-apache_"$buildnumber" 5.6-apache
-docker build -q -t "$1"/php:7.0-apache_"$buildnumber" 7.0-apache
-docker build -q -t "$1"/php:7.2-apache_"$buildnumber" -t 7.2-apache
-docker build -q -t "$1"/php:7.3-apache_"$buildnumber" -t "$1"/php:latest_"$buildnumber" 7.3-apache
-docker tag "$1"/php:latest_"$buildnumber" "$1"/php:latest
-
-docker push "$1"/php:5.6-apache_"$buildnumber"
-docker push "$1"/php:7.0-apache_"$buildnumber"
-docker push "$1"/php:7.2-apache_"$buildnumber"
-docker push "$1"/php:7.3-apache_"$buildnumber"
-docker push "$1"/php:latest_"$buildnumber"
-docker push "$1"/php:latest
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx create --name samplekit
+docker buildx use samplekit
+docker buildx inspect --bootstrap
+docker buildx build -t "$1"/php:5.6-apache_"$buildnumber" --platform linux/arm64,linux/amd64 --push 5.6-apache
+docker buildx build -t "$1"/php:7.0-apache_"$buildnumber" --platform linux/arm64,linux/amd64 --push 7.0-apache
+docker buildx build -t "$1"/php:7.2-apache_"$buildnumber" --platform linux/arm64,linux/amd64 --push 7.2-apache
+docker buildx build -t "$1"/php:7.3-apache_"$buildnumber" -t "$1"/php:latest_"$buildnumber" -t "$1"/php:latest --platform linux/arm64,linux/amd64 --push 7.3-apache
 
 # xdebug depends on base images
 # generate dockerfile for xdebug
@@ -31,17 +27,9 @@ sed -e s/reponame/"$1"/g -e s/buildnumber/"$buildnumber"/g 7.2-apache-xdebug/Doc
 sed -e s/reponame/"$1"/g -e s/buildnumber/"$buildnumber"/g 7.3-apache-xdebug/Dockerfile.template > 7.3-apache-xdebug/Dockerfile
 
 # build xdebug images
-docker build -q -t "$1"/5.6-apache-xdebug_"$buildnumber" 5.6-apache-xdebug
-docker build -q -t "$1"/7.0-apache-xdebug_"$buildnumber" 7.0-apache-xdebug
-docker build -q -t "$1"/7.2-apache-xdebug_"$buildnumber" 7.2-apache-xdebug
-docker build -q -t "$1"/7.3-apache-xdebug_"$buildnumber" -t "$1"/php:latest-xdebug_"$buildnumber" 7.3-apache-xdebug
-docker tag "$1"/php:latest-xdebug_"$buildnumber" "$1"/php:latest-xdebug
-
-docker push "$1"/php:5.6-apache-xdebug_"$buildnumber"
-docker push "$1"/php:7.0-apache-xdebug_"$buildnumber"
-docker push "$1"/php:7.2-apache-xdebug_"$buildnumber"
-docker push "$1"/php:7.3-apache-xdebug_"$buildnumber"
-docker push "$1"/php:latest-xdebug_"$buildnumber"
-docker push "$1"/php:latest-xdebug
-
+docker buildx build -t "$1"/5.6-apache-xdebug_"$buildnumber" --platform linux/arm64,linux/amd64 --push 5.6-apache-xdebug
+docker buildx build -t "$1"/7.0-apache-xdebug_"$buildnumber" --platform linux/arm64,linux/amd64 --push 7.0-apache-xdebug
+docker buildx build -t "$1"/7.2-apache-xdebug_"$buildnumber" --platform linux/arm64,linux/amd64 --push 7.2-apache-xdebug
+docker buildx build -t "$1"/7.3-apache-xdebug_"$buildnumber" -t "$1"/php:latest-xdebug_"$buildnumber" -t "$1"/php:latest-xdebug --platform linux/arm64,linux/amd64 --push 7.3-apache-xdebug
+docker buildx rm samplekit 
 docker logout
